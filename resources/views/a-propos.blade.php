5c6b5c6a00e3edacee418807403982464fef4434
NECR0M4NIA <gary.necromania@gmail.com>
2026-06-17 14:44:35 +0200

    Added Pages (WIP)

---
+++ resources/views/a-propos.blade.php
@@ -0,0 +1,60 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            À Propos
+        </h2>
+    </x-slot>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-12">
+        <h1 class="text-6xl text-white text-center my-[2em]">L’équipe du projet</h1>
+        <div class="flex flex-row justify-between bg-gray-500 rounded-xl px-12 py-4">
+            <img src="/assets/images/peoples/jajoudev.png" alt="">
+            <div class="w-1/2 flex flex-col">
+                <h2>JajouDev</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat quas officia magni ea lorem irure occaecat duis obcaecati
+                    vitae ea nesciunt. Dolores inventore architecto sed aute commodo cillum enim proident. Voluptas dolorem dolorem ex sint magnamquaerat </p>
+                <div>
+                    <a href="">Github</a>
+                    <a href="">YouTube</a>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row-reverse justify-between bg-violet-950 rounded-xl px-12 py-4">
+            <img src="/assets/images/peoples/forcerion.png" alt="">
+            <div class="w-1/2 flex flex-col">
+                <h2>JajouDev</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat quas officia magni ea lorem irure occaecat duis obcaecati
+                    vitae ea nesciunt. Dolores inventore architecto sed aute commodo cillum enim proident. Voluptas dolorem dolorem ex sint magnamquaerat </p>
+                <div>
+                    <a href="">Github</a>
+                    <a href="">YouTube</a>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row justify-between bg-[#D5DD89] rounded-xl px-12 py-4">
+            <img src="/assets/images/peoples/fonceurok.png" alt="">
+            <div class="w-1/2 flex flex-col">
+                <h2>JajouDev</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat quas officia magni ea lorem irure occaecat duis obcaecati
+                    vitae ea nesciunt. Dolores inventore architecto sed aute commodo cillum enim proident. Voluptas dolorem dolorem ex sint magnamquaerat </p>
+                <div>
+                    <a href="">Github</a>
+                    <a href="">YouTube</a>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row-reverse justify-between bg-blue-950 rounded-xl px-12 py-4">
+            <img src="/assets/images/peoples/necromania.png" alt="">
+            <div class="w-1/2 flex flex-col">
+                <h2>JajouDev</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat quas officia magni ea lorem irure occaecat duis obcaecati
+                    vitae ea nesciunt. Dolores inventore architecto sed aute commodo cillum enim proident. Voluptas dolorem dolorem ex sint magnamquaerat </p>
+                <div>
+                    <a href="">Github</a>
+                    <a href="">YouTube</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
